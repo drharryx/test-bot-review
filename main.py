5b819cb4f8294bf728a8d2169ca2795c9fe9d112
drharryx <alvarovarela.1@gmail.com>
2024-07-17 17:57:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-# Este es un programa para calcular el promedio de notas
+from flask import Flask
 
-def calcular_promedio(notas):
-    suma = 0
-    for i in range(0, len(notas)):
-        suma += notas[i]
-    promedio = suma / len(notas)
-    return promedio
+app = Flask(__name__)
 
-def imprimir_resultado(promedio):
-    if promedio >= 6:
-        print("Aprobado")
-    elif promedio >= 4:
-        print("Recuperación")
-    else:
-        print("Reprobado")
+@app.route('/')
+def hello_world():
+    return 'Hola, Mundo'
 
-notas = []
-while True:
-    nota = input("Ingrese una nota (o 'q' para terminar): ")
-    if nota == 'q':
-        break
-    notas.append(float(nota))
-
-promedio = calcular_promedio(notas)
-print("El promedio es: " + str(promedio))
-imprimir_resultado(promedio)
-
-a = 10
-b = "20"
-c = a + b
+if __name__ == '__main__':
+    app.run(debug=True)
